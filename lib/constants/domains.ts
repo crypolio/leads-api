077a98fd35afc61d28c973712c083ead7a0635a6
Choleski Louis <louischoleski@gmail.com>
2024-10-13 13:38:51 -0400

    Update whitelisted domains.

--- lib/constants/domains.ts
+++ lib/constants/domains.ts
@@ -1,10 +1,10 @@
 export const WHITELISTED_DOMAINS = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://www.secure.tigado.ca",
-  "https://www.staging.tigado.ca"
+  "https://www.readeasygen.com",
+  "https://www.staging.leadeasygen.com"
 ];
 
-export const REDIRECT_URL = "https://www.tigado.ca";
+export const REDIRECT_URL = "https://www.leadeasygen.com";
 
 export default WHITELISTED_DOMAINS;
